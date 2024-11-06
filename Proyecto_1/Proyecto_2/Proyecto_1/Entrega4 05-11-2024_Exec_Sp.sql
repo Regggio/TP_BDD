31227c15c077fd5875e04594f654c11b3d5987f2
Regggio <111200473+Regggio@users.noreply.github.com>
2024-11-06 12:49:00 -0300

    Add files via upload

---
+++ Proyecto_1/Proyecto_2/Proyecto_1/Entrega4 05-11-2024_Exec_Sp.sql
@@ -0,0 +1,14 @@
+---ENTREGA 4
+---EJECUCION DE LOS SP PARA IMPORTACION DE ARCHIVOS
+
+GO
+exec grupo3.Importacion_catalogo 
+GO
+
+GO
+exec grupo3.Insercion_Suc
+GO
+
+GO
+exec grupo3.Insercion_Emp 
+GO
