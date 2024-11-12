f914a5da64f3995818520ad3374449f7eea2e074
Regggio <111200473+Regggio@users.noreply.github.com>
2024-11-12 20:29:53 -0300

    Add files via upload

---
+++ Proyecto_5/Proyecto_1/Entrega3 01 Creacion_SP_Tablas.sql
@@ -0,0 +1,628 @@
+USE Com2900G03
+------------
+--INSERTAR SUCURSAL
+GO
+CREATE OR ALTER PROCEDURE insert3.InsertarSucursal
+	@Ciudad varchar(30),
+	@Sucursal varchar(50),
+	@Direccion varchar(120),
+	@Horario varchar(70),
+	@Telefono char(15)
+AS
+BEGIN 
+	DECLARE @MSJ varchar(50) = '';
+
+	--Valdiar Ciudad
+	IF(@Ciudad IN (SELECT Ciudad FROM grupo3.Sucursal))
+BEGIN
+	SET @MSJ = @MSJ + 'CIUDAD YA EXISTENTE '
+END
+
+	--Validar Sucursal
+	IF(@MSJ = '' AND @Sucursal IN (SELECT Sucursal FROM grupo3.Sucursal))
+BEGIN
+	SET @MSJ = @MSJ + 'SUCURSAL YA EXISTENTE '
+END
+	---VALIDAR TAM DATOS
+	IF(@MSJ = '' AND LEN(@Ciudad) > 20)
+	SET @MSJ = @MSJ + 'TAM DE CIUDAD EXCEDE EL MAXIMO '
+
+	IF(@MSJ = '' AND LEN(@Sucursal) > 20)
+	SET @MSJ = @MSJ + 'TAM DE SUCURSAL EXCEDE EL MAXIMO '
+
+	IF(@MSJ = '' AND LEN(@Direccion) > 100)
+	SET @MSJ = @MSJ + 'TAM DE DIRECCION EXCEDE EL MAXIMO '
+
+	IF(@MSJ = '' AND LEN(@Horario)>60)
+	SET @MSJ = @MSJ + 'TAM DE HORARIO EXCEDE EL MAXIMO '
+
+	IF(@MSJ = '' AND LEN(@Telefono)>9)
+	SET @MSJ = @MSJ + 'TAM DE TELEFONO EXCEDE EL MAXIMO '
+
+	IF(@MSJ = '')
+BEGIN
+	INSERT INTO grupo3.Sucursal(Ciudad,Sucursal,Direccion,Horario,Telefono)
+	VALUES(@Ciudad,@Sucursal,@Direccion,@Horario,@Telefono)
+END
+	ELSE
+	PRINT @MSJ
+
+END
+GO
+
+--MODIFICAR SUCURSAL
+GO
+CREATE OR ALTER PROCEDURE update3.ModificarSucursal
+    @id_sucursal INT,
+	@Ciudad VARCHAR(20),
+	@Sucursal VARCHAR(20),
+    @Direccion VARCHAR(100),
+	@Horario varchar(60),
+	@Telofono char(9)
+	
+AS
+BEGIN
+	DECLARE @MSJ varchar(50) = '';
+
+	--Valdiar Ciudad
+	IF(@Ciudad IN (SELECT Ciudad FROM grupo3.Sucursal))
+BEGIN
+	SET @MSJ = @MSJ + 'CIUDAD YA EXISTENTE '
+END
+
+	--Validar Sucursal
+	IF(@MSJ = '' AND @Sucursal IN (SELECT Sucursal FROM grupo3.Sucursal))
+BEGIN
+	SET @MSJ = @MSJ + 'SUCURSAL YA EXISTENTE '
+END
+	IF(@MSJ = '')
+BEGIN
+    UPDATE grupo3.Sucursal
+    SET Ciudad = @Ciudad,
+		Sucursal = @Sucursal,
+		Direccion = @Direccion,
+		Horario = @Horario,
+		Telefono = @Telofono
+    WHERE id_sucursal = @id_sucursal;
+END
+ELSE
+	PRINT @MSJ
+END;
+GO
+
+-- sp para borrado logico 
+GO
+CREATE OR ALTER PROCEDURE delete3.BorrarSucursal
+    @id_sucursal INT
+AS
+BEGIN
+	IF(@id_sucursal IN (SELECT id_Sucursal FROM grupo3.Sucursal))
+BEGIN
+    UPDATE grupo3.Sucursal
+    SET Fechabaja = CONVERT (date, GETDATE())
+    WHERE id_sucursal = @id_sucursal;
+END
+ELSE
+	PRINT 'LA SUCURSAL QUE SE DESEA ELIMINAR NO EXISTE'
+END;
+GO
+
+---------------------INSERTAR EMPLEADO
+CREATE OR ALTER PROCEDURE insert3.InsertarEmpleado
+	@Nombre varchar (50),
+	@Apellido varchar (50),
+	@DNI int,
+	@Direccion varchar (120),
+	@email_Personal varchar(80),
+	@email_Empresa varchar(80),
+	@CUIL char(14),
+	@Cargo varchar (50),
+	@id_Sucursal int,
+	@Turno varchar(40)
+AS
+BEGIN
+DECLARE @MSJ varchar(50) = '';
+
+	--Valdiar Ciudad
+	IF(@id_Sucursal NOT IN (SELECT id_sucursal FROM grupo3.Sucursal))
+	SET @MSJ = @MSJ + 'SUCURSAL INEXISTENTE'
+
+	--Valdiar Turno
+	IF(@MSJ = '' AND @Turno NOT IN ('TM','TT','Jornada Completa'))
+	SET @MSJ = @MSJ + 'TURNO INVALIDO'
+
+	--Valida Tam
+	IF(@MSJ = '' AND LEN(@Nombre) > 40)
+	SET @MSJ = @MSJ + 'TAM DEL NOMBRE EXCEDE EL MAXIMO'
+
+	IF(@MSJ = '' AND LEN(@Apellido) > 40)
+	SET @MSJ = @MSJ + 'TAM DE APELLIDO EXCEDE EL MAXIMO'
+
+	IF(@MSJ = '' AND LEN(@Direccion) > 100)
+	SET @MSJ = @MSJ + 'TAM DE DIRECCION EXCEDE EL MAXIMO'
+
+	IF(@MSJ = '' AND LEN(@email_Personal)>60)
+	SET @MSJ = @MSJ + 'TAM DE EMAIL PERSONAL EXCEDE EL MAXIMO'
+
+	IF(@MSJ = '' AND LEN(@email_Empresa)>60)
+	SET @MSJ = @MSJ + 'TAM DE EMAIL DE LA EMPRESA EXCEDE EL MAXIMO'
+
+	IF(@MSJ = '' AND LEN(@CUIL) > 11)
+	SET @MSJ = @MSJ + 'TAM DEL CUIL EXCEDE EL MAXIMO'
+
+	IF(@MSJ = '' AND LEN(@Cargo) > 30)
+	SET @MSJ = @MSJ + 'TAM DEL CARGO EXCEDE EL MAXIMO'
+
+	IF(@MSJ = '' AND LEN(@Turno) > 20)
+	SET @MSJ = @MSJ + 'TAM DE TURNO EXCEDE EL MAXIMO'
+
+	IF(@MSJ = '')
+BEGIN
+	INSERT INTO grupo3.Empleado(Nombre,Apellido,DNI,Direccion, email_Personal,email_Empresa,CUIL,Cargo,id_Sucursal,Turno)
+	VALUES(@Nombre,@Apellido,@DNI,@Direccion, @email_Personal,@email_Empresa,@CUIL,@Cargo,@id_Sucursal,@Turno)
+END
+	ELSE
+	PRINT @MSJ
+END
+
+
+
+
+----------------------MODIFICAR EMPLEADO
+GO
+CREATE OR ALTER PROCEDURE update3.ModificarEmpleado
+    @id_empleado INT,
+	@Nombre varchar(40),
+	@Apellido varchar (40),
+	@DNI int,
+	@Direccion varchar (100),
+	@email_Personal varchar(60),
+	@email_Empresa varchar(60),
+    @CUIL char(11),
+	@Cargo varchar (30),
+	@id_sucursal int,
+	@Turno varchar(20)
+AS
+BEGIN
+	if(@id_sucursal IN (SELECT id_sucursal FROM grupo3.Sucursal))
+	BEGIN
+    UPDATE grupo3.Empleado
+    SET Nombre = @Nombre,
+		Apellido = @Apellido,
+		DNI = @DNI,
+		Direccion = @Direccion,
+		email_Personal = @email_Personal,
+		email_Empresa = @email_Empresa,
+		CUIL= @CUIL, 
+		Cargo = @Cargo,
+		id_Sucursal=@id_sucursal,
+		Turno = @Turno
+    WHERE id_Empleado = @id_empleado;
+	END
+	ELSE
+	PRINT 'NUMERO DE SUCURSAL INEXISTENTE'
+END;
+
+----------------------BORRAR EMPLEADO
+GO
+CREATE OR ALTER PROCEDURE delete3.BorrarEmpleado
+    @id_empleado INT
+AS
+BEGIN
+	IF(@id_empleado IN (SELECT id_Empleado FROM grupo3.Empleado))
+BEGIN
+    UPDATE grupo3.Empleado
+    SET Fechabaja = CONVERT (date, GETDATE())
+    WHERE id_Empleado = @id_empleado;
+END
+ELSE
+	PRINT 'EL EMPLEADO QUE SE DESEA ELIMINAR NO EXISTE'
+END;
+GO
+
+----------------------INSERRTAR PRODUCTO
+CREATE OR ALTER PROCEDURE insert3.InsertarProducto
+	@Categoria varchar(50),
+	@Linea_De_Producto varchar(30),
+	@Nombre varchar(110),
+	@Precio_Unitario decimal(10,2),
+	@Precio_De_Referencia decimal(10,2),
+	@Unidad_De_Referencia varchar(30)
+AS
+BEGIN
+DECLARE @MSJ varchar(50) = '';
+
+	---Validar precio positivo
+	IF(@Precio_Unitario <= 0)
+	SET @MSJ = @MSJ + 'EL PRECIO UNITARIO DEBE SER POSITIVO'
+
+	IF(@MSJ = '' AND @Precio_De_Referencia <= 0)
+	SET @MSJ = @MSJ + 'EL PRECIO DE REFERENCIA DEBE SER POSTIVO'
+
+	--Valdiar Tam
+	IF(@MSJ = '' AND LEN(@Categoria) > 40)
+	SET @MSJ = @MSJ + 'TAM DEL CATEGORIA EXCEDE EL MAXIMO'
+
+	IF(@MSJ = '' AND LEN(@Linea_De_Producto) > 25)
+	SET @MSJ = @MSJ + 'TAM DE LINEA DE PRODUCTO EXCEDE EL MAXIMO'
+
+	IF(@MSJ = '' AND LEN(@Nombre) > 100)
+	SET @MSJ = @MSJ + 'TAM DE NOMBRE EXCEDE EL MAXIMO'
+
+	IF(@MSJ = '' AND LEN(@Unidad_De_Referencia) > 25)
+	SET @MSJ = @MSJ + 'TAM DE UNIDAD DE REFERENCIA EXCEDE EL MAXIMO'
+
+	IF(@MSJ = '')
+BEGIN
+	INSERT INTO grupo3.Producto(Categoria,Linea_De_Producto,Nombre,Precio_Unitario,
+								Precio_De_Referencia,Unidad_De_Referencia,FechaAlta)
+	VALUES(@Categoria,@Linea_De_Producto,@Nombre,@Precio_Unitario,
+								@Precio_De_Referencia,@Unidad_De_Referencia,CONVERT (date, GETDATE()))
+END
+	ELSE
+	PRINT @MSJ
+
+END
+----------------------MODIFICAR PRODUCTO
+GO
+CREATE OR ALTER PROCEDURE update3.ModificarProducto
+    @id_producto INT, 
+	@categoria varchar (40),
+	@linea_prod varchar(25),
+	@nombre varchar(100),
+	@precio_unitario decimal (10,2), 
+	@precio_referencia decimal(10,2),
+	@Unidad_De_Referencia varchar(25)
+AS
+BEGIN
+DECLARE @MSJ varchar(50) = '';
+
+	---Validar precio positivo
+	IF(@Precio_Unitario <= 0)
+	SET @MSJ = @MSJ + 'EL PRECIO UNITARIO DEBE SER POSITIVO'
+
+	IF(@MSJ = '' AND @precio_referencia <= 0)
+	SET @MSJ = @MSJ + 'EL PRECIO DE REFERENCIA DEBE SER POSTIVO'
+
+	IF(@MSJ = '')
+BEGIN
+    UPDATE grupo3.Producto
+    SET Linea_De_Producto= @linea_prod , 
+		Nombre=@nombre ,
+		Categoria=@categoria,
+		Precio_Unitario=@precio_unitario , 
+		Precio_De_Referencia=@precio_referencia ,
+		Unidad_De_Referencia = @Unidad_De_Referencia,
+		FechaAlta= CONVERT (date, GETDATE())
+    WHERE id_Producto = @id_producto ;
+END
+ELSE
+	PRINT @MSJ
+END;
+GO
+----------------------BORRAR PRODUCTO
+GO
+CREATE OR ALTER PROCEDURE delete3.BorrarProducto
+    @id_producto INT
+AS
+BEGIN
+ 	IF(@id_producto IN (SELECT id_Producto FROM grupo3.Producto))
+BEGIN
+    UPDATE grupo3.Producto
+    SET Fechabaja = CONVERT (date, GETDATE())
+    WHERE id_Producto = @id_producto;
+END
+ELSE
+	PRINT 'EL PRODUCTO QUE SE DESEA ELIMINAR NO EXISTE'
+END;
+GO
+
+----------------------INSERTAR VENTA
+GO
+CREATE OR ALTER PROCEDURE insert3.InsertarVenta
+	@Tipo_Cliente varchar(10),
+	@Genero varchar(10),
+	@id_Empleado int,
+	@Numero_Fac char(11)
+
+AS
+BEGIN
+DECLARE @MSJ varchar(50) = ''
+		---VALIDACION GENERO
+	IF(@Genero NOT IN ('Male','Female'))
+	SET @MSJ = @MSJ + 'EL GENERO INGRESADO NO ES VALIDO'
+
+	---VALIDACION TIPO CLIENTE
+	IF(@MSJ = '' AND @Tipo_Cliente NOT IN ('Normal','Member'))
+	SET @MSJ = @MSJ + 'EL TIPO DE CLIENTE INGRESADO NO ES VALIDO'
+
+	---VALIDACION DE EMPLEADO 
+	IF(@MSJ = '' AND @id_empleado NOT IN (SELECT id_Empleado FROM grupo3.Empleado))
+	SET @MSJ = @MSJ + 'ID DE EMPLEADO INEXISTENTE'
+
+	---VALIDACION DE FACTURA
+	IF(@MSJ = '' AND @Numero_Fac not IN (SELECT Numero_Fac FROM grupo3.Factura))
+	SET @MSJ = @MSJ + 'NO EXISTE TAL FACTURA'
+
+	IF(@MSJ ='')
+BEGIN
+	INSERT INTO grupo3.Venta(Tipo_Cliente,Genero,id_Sucursal,id_Empleado,id_Factura,Fecha)
+	VALUES(@Tipo_Cliente,
+			@Genero,
+			(SELECT id_Sucursal FROM grupo3.Empleado WHERE id_Empleado = @id_Empleado),
+			@id_Empleado,
+			(SELECT id_Factura FROM grupo3.Factura WHERE Numero_Fac = @Numero_Fac),
+			CONVERT(DATE, GETDATE()))
+END
+ELSE
+	PRINT @MSJ		
+END
+GO
+
+----------------------MODIFICAR VENTA	
+CREATE OR ALTER PROCEDURE update3.ModificarVenta
+    @id_venta INT,
+	@Tipo_Cliente varchar(20),
+	@Genero varchar(20),
+	@id_empleado int
+
+AS
+BEGIN
+DECLARE @MSJ varchar(50) = ''
+
+	---VALIDACION GENERO
+	IF(@Genero NOT IN ('Male','Female'))
+	SET @MSJ = @MSJ + 'EL GENERO INGRESADO NO ES VALIDO'
+
+	---VALIDACION TIPO CLIENTE
+	IF(@MSJ = '' AND @Tipo_Cliente NOT IN ('Normal','Member'))
+	SET @MSJ = @MSJ + 'EL TIPO DE CLIENTE INGRESADO NO ES VALIDO'
+
+	---VALIDACION DE EMPLEADO 
+	IF(@MSJ = '' AND @id_empleado NOT IN (SELECT id_Empleado FROM grupo3.Empleado))
+	SET @MSJ = @MSJ + 'ID DE EMPLEADO INEXISTENTE'
+
+	IF(@MSJ = '')
+BEGIN
+    UPDATE grupo3.Venta
+    SET Tipo_Cliente = @Tipo_Cliente,
+		Genero = @Genero,
+		id_Sucursal = (SELECT id_Sucursal FROM grupo3.Empleado WHERE id_Empleado = @id_Empleado),
+		Fecha = CONVERT (date, GETDATE()),
+		id_empleado = @id_empleado 
+    WHERE id_Venta = @id_venta ;
+	
+END
+ELSE
+	PRINT @MSJ
+END
+GO
+----------------------BORRAR VENTA
+GO
+CREATE OR ALTER PROCEDURE delete3.BorrarVenta
+    @id_venta INT
+AS
+BEGIN
+ 	IF(@id_venta IN (SELECT id_Venta FROM grupo3.Venta))
+BEGIN
+
+    UPDATE grupo3.Venta
+    SET Fechabaja = CONVERT (date, GETDATE())
+    WHERE id_Venta = @id_venta;
+END
+ELSE
+	PRINT 'LA VENTA QUE SE DESEA ELIMINAR NO EXISTE'
+END;
+GO
+
+--------------------INSERTAR FACTURA (tambien genera la venta asociada a la misma)
+GO
+CREATE OR ALTER PROCEDURE insert3.InsertarFactura
+    @Numero_Factura Char(11),
+	@tipo_factura char(2),
+	@metodo_pago varchar (15),
+	@Identificador_de_pago varchar(30)
+AS
+BEGIN
+DECLARE @MSJ varchar(50) = ''
+	
+	---VALIDAR NUMERO FACTURA
+	IF(@Numero_Factura IN (SELECT Numero_Fac FROM grupo3.Factura))
+	SET @MSJ = @MSJ + 'EL NUMERO DE FACTURA INGRESADO YA EXISTE'
+
+	---VALIDAR TIPO DE FACTURA
+	IF(@MSJ = '' AND @tipo_factura NOT IN ('A','B','C','D','E','NC','ND'))
+	SET @MSJ = @MSJ + 'EL TIPO DE FACTURA INGRESADO NO ES VALIDO'
+
+	---VALIDAR METODO DE PAGO
+	IF(@MSJ = '' AND @metodo_pago NOT IN ('Cash','Credit card', 'Ewallet'))
+	SET @MSJ = @MSJ + 'EL METODO DE PAGO INGRESADO NO ES VALIDO'
+
+	IF(@MSJ = '')
+BEGIN
+	INSERT INTO grupo3.Factura(Numero_Fac,Tipo_Factura,Metodo_Pago,Identificador_de_pago,Fecha,Hora)
+	VALUES(@Numero_Factura,@tipo_factura,@metodo_pago,@Identificador_de_pago,CONVERT(DATE, GETDATE()), CONVERT(TIME, GETDATE()))
+END
+ELSE
+	PRINT @MSJ
+
+END
+
+GO
+
+----------------------MODIFICAR FACTURA	
+CREATE OR ALTER PROCEDURE update3.ModificarFactura
+    @id_factura INT,
+    @tipo_factura char(2),
+	@metodo_pago varchar (15),
+	@Identificador_de_pago varchar(30)
+AS
+BEGIN
+	IF(@metodo_pago IN ('Cash','Credit card', 'Ewallet'))
+BEGIN
+	IF(@tipo_factura IN ('A','B','C','D','E','NC','ND'))
+BEGIN
+    UPDATE grupo3.Factura
+    SET Tipo_Factura = @tipo_factura , 
+		Fecha =CONVERT (date, GETDATE()),
+		Hora=CONVERT (time, GETDATE()),
+		Metodo_Pago = @metodo_pago,
+		Identificador_de_pago = @Identificador_de_pago
+    WHERE id_Factura = @id_factura ;
+END
+ELSE
+	PRINT 'TIPO DE FACTURA INVALIDO'
+END
+ELSE
+	PRINT 'METODO DE PAGO INVALIDO'
+END;
+GO
+----------------------BORRAR FACTURA
+GO
+CREATE OR ALTER PROCEDURE delete3.BorrarFactura
+    @id_factura INT,
+	@Numero_Fac char(11)
+AS
+BEGIN
+	IF(@id_factura IN (SELECT id_Factura FROM grupo3.Factura))
+BEGIN
+	IF(@Numero_Fac NOT IN (SELECT Numero_Fac FROM grupo3.Factura))
+BEGIN
+---CREO LA NOTA DE CREDITO PARA LA FACTURA EN CUESTION
+	INSERT INTO grupo3.Factura (Tipo_Factura, Numero_Fac, Fecha, Hora)
+	VALUES('NC',@Numero_Fac,CAST(GETDATE() AS DATE),CAST(GETDATE() AS TIME))
+
+---CAMBIO PARA QUE LA VENTA AHORA ESTE RELACIONADA CON ESA NC
+	UPDATE grupo3.Venta
+	SET id_Factura = (SELECT id_factura FROM grupo3.Factura where Numero_Fac = @Numero_Fac)
+	WHERE id_Factura = @id_factura
+END
+ELSE
+	PRINT 'EL NUMERO DE FACTURA QUE DESEA INGRESAR  YA EXISTE EN EL SISTEMA'
+END
+ELSE
+	PRINT 'LA FACTURA A LA QUE DESEA APLCIAR LA NC NO EXISTE EN EL SISTEMA'
+END;
+GO
+
+
+
+----------------------------------------------------------------------------------------
+---INSERTAR DETALLE DE VENTA
+GO
+CREATE OR ALTER PROCEDURE insert3.Insertar_Detalle
+	@Cantidad int,
+	@Nombre_Producto varchar(110),
+	@id_Venta int 
+AS
+BEGIN 
+DECLARE @MSJ varchar(50) = ''
+DECLARE @id_Producto int
+DECLARE @Precio_Unitario decimal(10,2)
+---VALIDAR EXISTENCIA DE LA VENTA
+	IF(@id_Venta NOT IN (SELECT id_Venta FROM grupo3.Venta))
+	SET @MSJ = @MSJ + 'ID DE VENTA INEXISTENTE'
+
+---VALIDAR EXISTENCIA DEL PRODUCTO
+	IF(@MSJ = '' AND @Nombre_Producto NOT IN (SELECT Nombre FROM grupo3.Producto))
+	SET @MSJ = @MSJ + 'EL PRODUCTO INGRESADO ES INEXISTENTE'
+	ELSE
+BEGIN
+	SET @id_Producto = (SELECT id_Producto FROM grupo3.Producto WHERE Nombre = @Nombre_Producto)
+	SET @Precio_Unitario = (SELECT Precio_Unitario FROM grupo3.Producto WHERE Nombre = @Nombre_Producto)
+END
+
+---VALIDAR QUE LA CANTIDAD SEA POSITIVA
+	IF(@MSJ = '' AND @Cantidad <= 0 OR @Cantidad = NULL)
+	SET @MSJ = @MSJ + 'LA CANTIDAD INGRESADA NO ES VALIDA'
+
+	IF(@MSJ = '')
+BEGIN
+	INSERT INTO grupo3.Detalle_Venta(Cantidad,id_Producto,id_Venta,Precio_Unitario)
+	VALUES(@Cantidad,@id_Producto,@id_Venta,@Precio_Unitario)
+
+END
+ELSE
+PRINT @MSJ
+
+END
+GO
+
+---MODIFICAR DETALLE DE VENTA
+GO
+CREATE OR ALTER PROCEDURE update3.Modificar_Detalle
+	@id_Detalle int,
+	@Cantidad int,
+	@Nombre_Producto varchar(110),
+	@id_Venta int 
+AS
+BEGIN
+	IF(@id_Venta IN (SELECT id_Venta FROM grupo3.Venta))
+BEGIN 
+	IF(@Nombre_Producto IN (SELECT Nombre FROM grupo3.Producto))
+BEGIN
+	IF(@Cantidad > 0)
+BEGIN
+	UPDATE grupo3.Detalle_Venta
+	SET Cantidad = @Cantidad,
+		id_Producto = (SELECT id_Producto FROM grupo3.Producto WHERE Nombre = @Nombre_Producto),
+		Precio_Unitario = (SELECT Precio_Unitario FROM grupo3.Producto WHERE Nombre = @Nombre_Producto),
+		id_Venta = @id_Venta
+	WHERE id_Detalle = @id_Detalle
+		
+END
+ELSE
+PRINT 'CANTIDAD INGRESADA INVALIDA'
+
+END
+ELSE
+PRINT 'EL PRODUCTO INGRESADO ES INEXISTENTE'
+
+END
+ELSE
+PRINT 'ID DE VENTA INEXISTENTE'
+
+END
+GO
+
+---BORARR DETALLE DE VENTA
+GO
+CREATE OR ALTER PROCEDURE delete3.Borrar_Detalle
+	@id_Detalle int
+AS
+BEGIN 
+	 IF(@id_Detalle IN (SELECT id_Detalle FROM grupo3.Detalle_Venta))
+BEGIN
+
+    UPDATE grupo3.Detalle_Venta
+    SET Fechabaja = CONVERT (date, GETDATE())
+    WHERE id_Detalle = @id_Detalle;
+END
+ELSE
+	PRINT 'EL DETALLE DE VENTA QUE DESEA ELIMINAR NO EXISTE'
+
+END
+GO
+
+GO 
+CREATE OR ALTER PROCEDURE insert3.FacturayVenta
+	@Numero_Factura Char(11),
+	@tipo_factura char(2),
+	@metodo_pago varchar (15),
+	@Identificador_de_pago varchar(30),
+	@Tipo_Cliente varchar(10),
+	@Genero varchar(10),
+	@id_Empleado int,
+	@Cantidad int,
+	@Nombre_Producto varchar(110),
+	@id_Venta int
+AS
+BEGIN 
+
+EXEC insert3.InsertarFactura @Numero_Factura, @tipo_factura,@metodo_pago,@Identificador_de_pago 
+
+EXEC insert3.InsertarVenta @Tipo_Cliente, @Genero, @id_Empleado, @Numero_Factura
+
+EXEC insert3.Insertar_Detalle @Cantidad, @Nombre_Producto, @id_Venta
+
+END
---
+++ Proyecto_5/Proyecto_1/Reportes_Creacion_SP.sql
@@ -0,0 +1,152 @@
+--CREACION DE SP PARA LOS REGISTROS
+GO
+	USE Com2900G03
+GO
+
+--CREACION REPORTE Trimestral: mostrar el total facturado por turnos de trabajo por mes.
+GO
+CREATE or ALTER PROCEDURE grupo3.ReporteTrimestral (@trimestre int, @anio int)
+as
+begin
+SELECT
+	MONTH(v.fecha) as mes, 
+	e.Turno, sum(v.total) --over (partition by month(v.fecha), e.turno order by month(v.fecha)) as TotalMensual
+	FROM grupo3.Venta v 
+	inner join grupo3.Empleado e on v.id_Empleado=e.id_Empleado
+	WHERE 
+        YEAR(v.fecha) = @anio 
+        AND (
+            (@trimestre = 1 AND MONTH(v.fecha) BETWEEN 1 AND 3) OR
+            (@trimestre = 2 AND MONTH(v.fecha) BETWEEN 4 AND 6) OR
+            (@trimestre = 3 AND MONTH(v.fecha) BETWEEN 7 AND 9) OR
+            (@trimestre = 4 AND MONTH(v.fecha) BETWEEN 10 AND 12)
+        )
+	GROUP BY month(v.fecha), Turno
+FOR XML PATH('ReporteTrimestral');
+end
+GO
+
+---CREACION REPORTE Mensual: ingresando un mes y año determinado mostrar el total facturado por días de la semana, incluyendo sábado y domingo.
+GO
+CREATE or ALTER PROCEDURE grupo3.ReporteMensual (@mes int, @anio int)
+as
+begin
+SELECT 
+    DATENAME(WEEKDAY, fecha) AS DiaSemana,
+    SUM(Total) AS TotalFacturado
+FROM 
+    grupo3.Venta v
+WHERE 
+    MONTH(fecha) = @mes AND YEAR(fecha) = @anio
+GROUP BY 
+    DATENAME(WEEKDAY, fecha)
+FOR XML PATH('ReporteMensual');
+end
+GO
+
+/*CREACION REPORTE Por rango de fechas: ingresando un rango de fechas a demanda, debe poder mostrar
+la cantidad de productos vendidos en ese rango, ordenado de mayor a menor.*/
+GO
+CREATE or ALTER PROCEDURE grupo3.ReporteRango (@fechaI date, @fechaF date)
+as
+begin
+SELECT
+	p.id_Producto, p.Nombre,
+	SUM(v.cantidad) OVER (PARTITION BY v.id_Producto) AS TotalVendidos
+FROM
+	grupo3.Producto p
+	inner join grupo3.Venta v on p.id_Producto = v.id_Producto
+WHERE
+	v.fecha BETWEEN @fechaI AND @fechaF
+ORDER BY
+	TotalVendidos DESC
+FOR XML PATH('ReporteRango');
+end
+
+GO
+
+/*CREACION REPORTE Por rango de fechas: ingresando un rango de fechas a demanda, debe poder mostrar
+la cantidad de productos vendidos en ese rango por sucursal, ordenado de mayor a menor*/
+GO
+CREATE or ALTER PROCEDURE grupo3.ReporteRangoSucursal (@fechaI date, @fechaF date)
+as
+begin
+SELECT
+	s.Ciudad, p.id_Producto, p.Nombre,
+	SUM(v.cantidad) OVER (PARTITION BY v.id_Producto,s.Ciudad) AS TotalVendidosSucursal
+FROM
+	grupo3.Producto p
+	inner join grupo3.Venta v on p.id_Producto = v.id_Producto
+	inner join grupo3.Empleado e on v.id_Empleado = e.id_Empleado
+	inner join grupo3.Sucursal s on e.id_Sucursal = s.id_sucursal
+WHERE
+	v.fecha BETWEEN @fechaI AND @fechaF
+ORDER BY
+	TotalVendidosSucursal DESC
+FOR XML PATH('ReporteRangoSucursal');
+end
+GO
+
+/*CREACION REPORTE Mostrar los 5 productos más vendidos en un mes, por semana*/
+GO
+CREATE or ALTER PROCEDURE grupo3.ReporteSemanal (@mes int, @anio int)
+as 
+begin
+	WITH VentasSemanales (id_Producto,Nombre,Semana,CantidadSemanal,Top5) AS (
+		SELECT p.id_Producto, p.Nombre,
+			NTILE (4) OVER (PARTITION BY DAY (v.fecha) order by DAY (v.fecha)) AS Semana,
+			SUM(v.Cantidad) OVER (PARTITION BY v.id_Producto, WEEK(v.fecha)) AS CantidadSemanal,
+			RANK () OVER (PARTITION BY WEEK(v.fecha) ORDER BY CantidadSemanal DESC) AS Top5
+		FROM
+			grupo3.Venta v
+			inner join grupo3.Producto p on v.id_Producto = p.id_Producto
+		WHERE 
+			MONTH (v.fecha) = @mes AND YEAR (v.fecha) = @anio)
+	SELECT *
+	FROM VentasSemanales
+	WHERE Top5 <= 5
+FOR XML PATH('ReporteSemanal');
+end
+GO
+
+GO
+CREATE or ALTER PROCEDURE grupo3.ReporteMenosVendidos (@mes int, @anio int)
+as
+begin
+	WITH VentasMensuales (id_Producto,Nombre,CantidadMensual) AS (
+		SELECT p.id_Producto, p.Nombre,
+		SUM(v.Cantidad) OVER (PARTITION BY v.id_Producto) AS CantidadMensual,
+		RANK() OVER (ORDER BY CantidadMensual ASC) AS Top5
+		FROM
+			grupo3.Venta v
+			inner join grupo3.Producto p on v.id_Producto = p.id_Producto
+			WHERE 
+            MONTH(v.fecha) = @mes AND YEAR(v.fecha) = @anio)
+	SELECT *
+	FROM VentasMensuales
+	WHERE Top5 <= 5
+FOR XML PATH('ReporteMenosVendidos');
+end
+GO
+
+GO
+CREATE or ALTER PROCEDURE grupo3.VentasFechaSucursal (@fecha date, @sucursal int)
+as
+begin
+	SELECT
+	s.Ciudad, v.fecha, p.id_Producto, p.Nombre,
+	SUM (v.Cantidad) OVER (PARTITION BY v.id_Producto) as CantidadVendida,
+	SUM (v.Total) OVER (PARTITION BY v.id_Producto) as TotalVendido
+	FROM 
+        grupo3.Venta v
+    inner join
+        grupo3.Producto p ON v.id_Producto = p.id_Producto
+    inner join 
+        grupo3.Empleado e ON v.id_Empleado = e.id_Empleado
+    inner join
+        grupo3.Sucursal s ON e.id_Sucursal = s.id_sucursal
+    WHERE 
+        v.fecha = @fecha AND s.id_sucursal = @sucursal
+FOR XML PATH('VentasFechaSucursal');
+end
+GO
