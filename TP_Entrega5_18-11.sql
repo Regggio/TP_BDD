7b7b64516387ef4fcd54a3f9b82f81a04fba5052
Facu <fgoldring@alumno.unlam.edu.ar>
2024-11-21 20:52:18 -0300

    TP_Entrega5_AsignacionDeRoles_21-11

--- TP_Entrega5_18-11.sql
+++ TP_Entrega5_18-11.sql
@@ -1,3 +1,16 @@
+--Crear usuario--
+CREATE LOGIN SupervisorLogin WITH PASSWORD = 'BaseDatos1';
+CREATE USER SupervisorUser FOR LOGIN SupervisorLogin;
+
+IF NOT EXISTS (SELECT * FROM sys.database_principals WHERE name = 'Supervisor')
+begin
+    CREATE ROLE Supervisor;
+end
+
+GRANT EXECUTE ON OBJECT::dbo.GenerarNotaDeCredito TO Supervisor;
+
+ALTER ROLE Supervisor ADD MEMBER SupervisorUser;
+
 CREATE or ALTER PROCEDURE grupo3.GenerarNotaDeCredito (@id_Usuario int, @id_Factura int, @Total decimal (10,2))
 as
 begin
@@ -6,14 +19,14 @@ begin
 
 	--Obtengo rol--
 	SELECT @Rol = rol FROM Usuarios WHERE id_Usuario = @id_Usuario;
-	--verifico que sea supervisor--
+	--Verifico que sea supervisor--
 	IF @Rol <> 'Supervisor'
 	begin
 		RAISERROR ('Solo los supervisores pueden generar notas de credito',16,1);
 		RETURN;
 	end
 
-	--OBtengo estado factura--
+	--Obtengo estado factura--
 	SELECT @EstadoFactura = Estado FROM Factura WHERE id_Factura = @id_Factura
 	--Verifico que este paga--
 	IF @EstadoFactura <> 'Pagada'
@@ -26,7 +39,7 @@ begin
 	INSERT INTO NotaDeCredito (id_Factura, Total) VALUES (@id_Factura, @Total);
 end
 
---Back UP--
+--BackUp--
 BACKUP DATABASE TP_Supermercado_GRPO3
 TO DISK = 'C:\Backups\TP_Supermercado_GRPO3.bak'
 WITH FORMAT,
