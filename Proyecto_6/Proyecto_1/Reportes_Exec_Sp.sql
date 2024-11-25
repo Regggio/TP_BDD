9b8edd68c5c9c2564a6e8f26ae92c9d2090d6096
Facu <fgoldring@alumno.unlam.edu.ar>
2024-11-25 20:52:01 -0300

    Reportes_Exec_Sp.sql

--- Proyecto_6/Proyecto_1/Reportes_Exec_Sp.sql
+++ Proyecto_6/Proyecto_1/Reportes_Exec_Sp.sql
@@ -8,8 +8,8 @@ EXECUTE grupo3.ReporteTrimestral 1, 2024
 
 EXECUTE grupo3.ReporteMensual 3,2024
 
-EXECUTE grupo3.ReporteRango '2024,01,01','2024,12,31'
+EXECUTE grupo3.ReporteRango '2024-01-01', '2024-12-31'
 
-EXECUTE grupo3.ReporteRangoSucursal '2024,01,01','2024,12,31'
+EXECUTE grupo3.ReporteRangoSucursal '2024-01-01', '2024-12-31'
 
 EXECUTE grupo3.ReporteSemanal 01,2024
